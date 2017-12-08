dd32347b218a4e17bbf9006ddf713f95947a87ef
Jared <jaredtaylor80@gmail.com>
2017-12-08 18:03:31 -0500

    get HTML elements and save as javascript variables

--- tower.js
+++ tower.js
@@ -1,18 +1,37 @@
-$(document).ready(function() {
+// $(document).ready(function() {
 
 // var pinkDisc = document.querySelector('#disc0')
 // var purpleDisc = document.querySelector('#disc1')
 // var blueDisc = document.querySelector('#disc2')
 
-const towerLeft = [3, 2, 1]
-// var leftElement = document.querySelector('#discs-left')
-// discsLeft = leftElement
+const towerLeft = []
+
 const towerMid = []
-// var midElement = document.querySelector('#discs-mid')
-// discsMid["midElement"] = midElement
+
 const towerRight = []
-// var rightElement = document.querySelector('#discs-right')
-// discsRight["rightElement"] = rightElement
+
+const tempTower = []
+
+let pinkDisc = document.querySelector('.disc0')
+console.log(pinkDisc)
+let purpleDisc = document.querySelector('.disc1')
+console.log(purpleDisc)
+let blueDisc = document.querySelector('.disc2')
+console.log(blueDisc)
+
+let leftTower = document.querySelector('.discs-left')
+let middleTower = document.querySelector('.discs-mid')
+let rightTower = document.querySelector('.discs-right')
+
+towerLeft.push(pinkDisc, purpleDisc)
+console.log(towerLeft)
+
+leftTower.removeChild(pinkDisc)
+rightTower.appendChild(pinkDisc)
 
 
-})
+
+
+
+
+// })
