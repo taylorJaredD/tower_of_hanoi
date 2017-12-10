49e334f632c34bf2f2b79ed797a022680bef8a15
Jared <jaredtaylor80@gmail.com>
2017-12-10 11:35:28 -0500

    add event listeners

--- tower.js
+++ tower.js
@@ -5,36 +5,72 @@
 // var blueDisc = document.querySelector('#disc2')
 
 const towerLeft = []
-
 const towerMid = []
-
 const towerRight = []
-
 const tempTower = []
-
+let clickedTower = document.querySelector('.discs')
 let pinkDisc = document.querySelector('.disc0')
-console.log(pinkDisc)
 let purpleDisc = document.querySelector('.disc1')
-console.log(purpleDisc)
 let blueDisc = document.querySelector('.disc2')
-console.log(blueDisc)
 
 let leftTower = document.querySelector('.discs-left')
 let middleTower = document.querySelector('.discs-mid')
 let rightTower = document.querySelector('.discs-right')
 
-towerLeft.push(pinkDisc, purpleDisc)
+towerLeft.push(pinkDisc, purpleDisc, blueDisc)
 console.log(towerLeft)
 
-leftTower.removeChild(pinkDisc)
-rightTower.appendChild(pinkDisc)
-
+// leftTower.removeChild(pinkDisc)
+// rightTower.appendChild(pinkDisc)
+// clickedTower.addEventListener("click", function(evt) {
+//   evt.preventDefault()
+// })
 leftTower.addEventListener("click", function(evt) {
   evt.preventDefault()
-  
+  // clicked()
+  firstClick()
+  secondClick()
 })
 
+middleTower.addEventListener("click", function(evt) {
+  evt.preventDefault()
+  // clicked()
+})
 
+rightTower.addEventListener("click", function(evt) {
+  evt.preventDefault()
+  // clicked()
+})
 
+function firstClick() {
+  let holder = towerLeft.pop()
+  console.log(holder)
+  leftTower.removeChild(pinkDisc)
+  holder = tempTower.push()
+  console.log(holder)
+}
 
+function secondClick() {
+  let holder = tempTower.pop()
+  console.log(tempTower)
+  let targetTower = towerRight.push()
+  console.log(targetTower)
+  rightTower.appendChild(pinkDisc)
+}
+
+// function clicked() {
+//   if (clickedTower === leftTower) {
+//     tempTower.pop()
+//     leftTower.push()
+//   } else if (clickedTower === middleTower) {
+//     tempTower.pop()
+//     middleTower.push()
+//   } else if (clickedTower === rightTower) {
+//     tempTower.pop()
+//     rightTower.push()
+//   }
+// // else {
+// //   invalidMove
+// // }
+// }
 // })
