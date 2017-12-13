fda2f572f81115ac0bb1cfff53202edfaf372a64
Jared <jaredtaylor80@gmail.com>
2017-12-13 18:23:14 -0500

    game functionality works

--- tower.js
+++ tower.js
@@ -1,7 +1,7 @@
 
 
-const towerLeft = []
-const towerMid = [3, 2, 1]
+const towerLeft = [3, 2, 1]
+const towerMid = []
 const towerRight = []
 
 let sourceTower
@@ -11,18 +11,20 @@ let hasClicked = false
 
 
 // let clickedTower = document.querySelector('.discs')
-// let pinkDisc = document.querySelector('.disc0')
-// let purpleDisc = document.querySelector('.disc1')
-// let blueDisc = document.querySelector('.disc2')
-//
+let pinkDisc = document.querySelector('.disc1')
+let purpleDisc = document.querySelector('.disc2')
+let blueDisc = document.querySelector('.disc3')
+
 let leftTower = document.querySelector('.discs-left')
 let middleTower = document.querySelector('.discs-mid')
 let rightTower = document.querySelector('.discs-right')
 
+let sourceDomTower
+let destDomTower
+
 function registerClick (evt) {
   evt.preventDefault()
   // console.log(this)
-  // clicked()
   if (!hasClicked) {
     // console.log('first click')
     firstClick(this)
@@ -38,10 +40,13 @@ function firstClick (element) {
   let source = element.className
   if (source === "discs-left") {
     sourceTower = towerLeft
+    sourceDomTower = leftTower
   } else if (source === "discs-mid") {
     sourceTower = towerMid
+    sourceDomTower = middleTower
   } else if (source === "discs-right") {
     sourceTower = towerRight
+    sourceDomTower = rightTower
   } else {
     console.alert("invalid tower clicked")
   }
@@ -52,10 +57,13 @@ function secondClick (element) {
   console.log(dest)
   if (dest === "discs-left") {
     destTower = towerLeft
+    destDomTower = leftTower
   } else if (dest === "discs-mid") {
     destTower = towerMid
+    destDomTower = middleTower
   } else if (dest === "discs-right") {
     destTower = towerRight
+    destDomTower = rightTower
   } else {
     console.alert("invalid tower clicked")
   }
@@ -64,10 +72,9 @@ function secondClick (element) {
 
 function checkRules () {
   let currentDisc = sourceTower[sourceTower.length - 1]
+  console.log(currentDisc)
   let topDestDisc = destTower[destTower.length - 1]
-
-
-
+  console.log(topDestDisc)
     if (currentDisc && destTower.length === 0) {
       doMove()
     } else if (currentDisc && topDestDisc && currentDisc < topDestDisc) {
@@ -79,9 +86,16 @@ function checkRules () {
 
   function doMove () {
     console.log("moving discs")
+    // sourceDomTower = source
+    let tempDisc = sourceTower.pop()
+    let tempDomDisc = document.querySelector(`.disc${tempDisc}`)
+    sourceDomTower.removeChild(tempDomDisc)
+    console.log(sourceTower)
+    destTower.push(tempDisc)
+    document.querySelector(`.disc${tempDisc}`)
+    destDomTower.insertBefore(tempDomDisc, destDomTower.childNodes[0])
+    console.log(destTower)
   }
-
-
 
 
 leftTower.addEventListener("click", registerClick)
